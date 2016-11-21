892ad2dff4f73fa857cff44c0253818ea80a32f0
Sebastian Hösl <sebastian.hoesl@outlook.de>
2016-11-21 08:42:39 +0100

    Added grunt tasks

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,1 +1,42 @@
+var src = './src/';
+var dest = './dist/';
 
+module.exports = function(grunt) {
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+
+        clean: [
+            dest
+        ],
+
+        copy: {
+            'js': {
+                flatten: true,
+                expand: true,
+                src: src + 'js/*.js',
+                dest: dest + 'js/'
+            }
+        },
+
+        uglify: {
+            main: {
+                options: {
+                    sourceMap: true
+                },
+
+                files: [{
+                    expand: true,
+                    cwd: dest + 'js/',
+                    src: ['*.js', '!*.min.js'],
+                    dest: dest + 'js',
+                    ext: '.min.js',
+                    extDot: 'last'
+                }]
+            }
+        }
+    });
+
+    require('load-grunt-tasks')(grunt);
+
+    grunt.registerTask('default', ['clean', 'copy', 'uglify']);
+};
